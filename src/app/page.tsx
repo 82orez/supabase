3174c8364f01162babd06978e0c4988ee9f1ff72
82orez <82orez@naver.com>
2024-12-23 16:44:40 +0900

    supabase utils

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,29 +4,6 @@ export default function Home() {
   return (
     <div>
       <div>Supabase's Home Page</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-      <div>Hello World</div>
     </div>
   );
 }
